c6dad78b2165ecd14d92cc77ea7d542e08590ee5
absjuniordev <abs.junnior@hotmail.com>
2022-08-12 00:01:56 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> lis = new List<Department>();
+            lis.Add(new Department { Id = 1, Name = "RH" });
+            lis.Add(new Department { Id = 2, Name = "ALA" });
+
+            return View(lis);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesWebMvc.Models
+{
+    [Table("Departiment")]
+    public class Department
+    {
+        [Column("Id")]
+        [Display(Name="Cod")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name="Name")]
+        public string Name { get; set; }
+    }
+}
