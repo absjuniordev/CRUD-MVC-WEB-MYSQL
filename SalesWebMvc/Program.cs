a36a1be6f05a903364cf54c364d4aecea8d0fcd6
absjuniordev <abs.junnior@hotmail.com>
2022-08-22 23:25:22 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
